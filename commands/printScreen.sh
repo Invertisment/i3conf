c0a8ae7d689f3abdfd4835a821f1fa1cd0050206
MartynasMaciulevicius <visiskaj.slaptaj@gmail.com>
2015-06-14 20:47:47 +0300

    Print for single window

--- commands/printScreen.sh
+++ commands/printScreen.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 # Take a screenshot and move it to Pictures
-scrot '%Y-%m-%d_%H:%M:%S_$wx$h.png' -e 'mv $f ~/Pictures/screenshots/'
+
+args=""
+title="%Y-%m-%d_%X.$(date +%s%N)"
+if [ "$1" == "-f" ]
+then
+    # scrot parameter: printscreen focused window
+    args="$args --focused"
+fi
+
+extension='.png'
+title="$title$extension"
+
+scrot $title -e 'mv $f ~/Pictures/screenshots/' $args
